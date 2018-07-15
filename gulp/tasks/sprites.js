a355f5e7e3dd9190126bac0d18854e0e0781d14b
biggydrink <andrew.butz@gmail.com>
2018-07-15 14:26:03 -0500

    added svg sprite package and set up sprites.js gulp task

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,7 +1,20 @@
 let gulp = require('gulp');
 let svgSprite = require('gulp-svg-sprite');
 
+let config = {
+    mode: {
+        css: {
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
+}
+
 gulp.task('createSprite', function() {
     return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite'));
 });
