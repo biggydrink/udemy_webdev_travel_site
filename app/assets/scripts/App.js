3713fdc5335c16b8c125ae60b2b49f086a9c3301
biggydrink <andrew.butz@gmail.com>
2018-09-21 17:36:20 -0500

    removed dummy js, added script to add class to menu content to make visible on click for small screens

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-// let $ = require('jquery'); // this ends up making webpack include node_modules/jquery/dist/jquery.js
-// let Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    
-    payTaxes() {
-        console.log(this.name + " now owes $0 in taxes");
-    }
-}
-
-
-let john = new Person("John Doe", "blue");
-let jane = new Adult("Jane Smith", "orange");
-
-john.greet();
-jane.greet();
-jane.payTaxes();
+let mobileMenu = new MobileMenu;
